d3ef61865b01c9d1959e2122f084a57b20bdefc5
Mitchell-D <mtd0012@uah.edu>
2025-07-18 13:25:22 -0500

    updated gridstats extractor to chunk spatially as well as temporally, now testing

--- emulate_era5_land/extract_gridstats.py
+++ emulate_era5_land/extract_gridstats.py
@@ -10,6 +10,7 @@ import json
 import gc
 import h5py
 from datetime import datetime
+from time import perf_counter
 from pathlib import Path
 from multiprocessing import Pool
 
@@ -18,12 +19,12 @@ from emulate_era5_land.helpers import _parse_feat_idxs,_calc_feat_array
 #from testbed.list_feats import nldas_record_mapping,noahlsm_record_mapping
 
 def _mp_get_gs_varsum(args):
-    return _get_gs_varsum(**args)
+    return args,_get_gs_varsum(**args)
 
 def _mp_get_gs_mmsc(args):
-    return _get_gs_mmsc(**args)
+    return args,_get_gs_mmsc(**args)
 
-def _get_gs_varsum(timegrid:Path, means:np.array, depermute=False,
+def _get_gs_varsum(timegrid:Path, means:np.array, spatial_slice=None,
         time_sector_size=None, derived_feats_serial:list=[]):
     """
     get the standard deviation of this timegrid's data per
@@ -32,11 +33,9 @@ def _get_gs_varsum(timegrid:Path, means:np.array, depermute=False,
 
     :@param timegrid: File from which to extract the data
     :@param means: (12,24,P,F) global mean values per month,hour,pixel,feat
-        combination used to calculate variance. If 'depermute' is set to True,
-        it is assumed that the means are already in de-permuted order, so the
-        stored variables will accordingly be depermuted before calculating.
-    :@param depermute: If True, extract the inverse spatial permutation
-        from the timegrid file and use it to reorder the extracted array
+        combination used to calculate variance.
+    :@param spatial_slice: slice object referring to the spatial dimension as
+        it is stored in the timegrid file which determines the extracted range.
     :@param time_sector_size: Number of elements along the time axis to
         extract at a time. Default to hdf5 chunk size along time axis.
     :@param derived_feats_serial: List of tuples like:
@@ -67,10 +66,10 @@ def _get_gs_varsum(timegrid:Path, means:np.array, depermute=False,
             tg_open["data"].attrs["static"])["flabels"]
     tg_static = tg_open["/data/static"][...]
     tg_dynamic = tg_open["/data/dynamic"]
-    if depermute:
-        inv_perm = tg_open["/data/permutation"][1]
-    else:
-        inv_perm = None
+    #if depermute:
+    #    inv_perm = tg_open["/data/permutation"][1]
+    #else:
+    #    inv_perm = None
     all_feats = [df[0] for df in derived_feats_serial] + list(tg_dlabels)
 
     ## determine sector slices along time axis
@@ -90,20 +89,31 @@ def _get_gs_varsum(timegrid:Path, means:np.array, depermute=False,
 
     ## construct a (M,T) counts array and (M,T,P,Fd,1) array for
     ## (min, max, sum) per combination of (month, ToD, pixel, feature)
-    varsum = np.zeros((12,24,*tg_shape[1:3],1), dtype=np.float64)
+    if spatial_slice is None:
+        spatial_slice = slice(0,tg_shape[1])
+    varsum_shape = (12,24,spatial_slice.stop-spatial_slice.start,tg_shape[2],1)
+    assert varsum_shape==means.shape
+    varsum = np.zeros(varsum_shape, dtype=np.float64)
     counts = np.zeros((12,24), dtype=int)
+
+    ## go ahead and extract the full time for this spatial area and close file
+    #tg_dynamic = tg_dynamic[:,spatial_slice]
+    #tg_static = tg_static[spatial_slice]
+    #tg_open.close()
+
     for slc in tslices:
-        tmp_arrs = []
+        tinit = perf_counter()
         ## evaluate derived features and reorder stored feats as requested.
         tmpx = _calc_feat_array(
-                src_array=tg_dynamic[slc], ## (T, P, Fd)
-                static_array=tg_static, ## (P, Fs)
+                src_array=tg_dynamic[slc,spatial_slice], ## (T, P, Fd)
+                static_array=tg_static[spatial_slice], ## (P, Fs)
                 stored_feat_idxs=sfix,
                 derived_data=drv_info,
                 )
+        assert np.all(np.isfinite(tmpx)),(timegrid.name,spatial_slice,slc)
         ## means must also be depermuted; do so before calculating the sum
-        if depermute:
-            tmpx = tmpx[:,inv_perm]
+        #if depermute:
+        #    tmpx = tmpx[:,inv_perm]
         midxs = tmp_months[slc]
         tidxs = tmp_tods[slc]
         for i in range(tmpx.shape[0]):
@@ -111,11 +121,12 @@ def _get_gs_varsum(timegrid:Path, means:np.array, depermute=False,
             counts[midxs[i],tidxs[i]] += 1
             tmp_means = means[midxs[i],tidxs[i],:,:]
             varsum[midxs[i],tidxs[i],:,:,0] += (tmpx[i]-tmp_means)**2
-        print(counts)
         gc.collect()
+        print(timegrid.name, spatial_slice, slc, f"{perf_counter()-tinit:.3f}")
+    tg_open.close()
     return (counts,varsum)
 
-def _get_gs_mmsc(timegrid:Path, depermute=False, time_sector_size=None,
+def _get_gs_mmsc(timegrid:Path, time_sector_size=None, spatial_slice=None,
         derived_feats_serial:list=[]):
     """
     get the minimum, maximum, sum, and counts of this timegrid's data per
@@ -123,10 +134,10 @@ def _get_gs_mmsc(timegrid:Path, depermute=False, time_sector_size=None,
     (12,24,P,F,3) array of floats and a (12,24) array of int counts.
 
     :@param timegrid: File from which to extract the data
-    :@param depermute: If True, extract the inverse spatial permutation
-        from the timegrid file and use it to reorder the extracted array
     :@param time_sector_size: Number of elements along the time axis to
         extract at a time. Default to hdf5 chunk size along time axis.
+    :@param spatial_slice: slice object referring to the spatial dimension as
+        it is stored in the timegrid file which determines the extracted range.
     :@param derived_feats_serial: List of tuples like:
         (dflabel:str, (dargs:list[str], sargs:list[str], lambda_str:str))
         which encode a way of calculating a new feature called {dflabel}
@@ -155,10 +166,10 @@ def _get_gs_mmsc(timegrid:Path, depermute=False, time_sector_size=None,
             tg_open["data"].attrs["static"])["flabels"]
     tg_static = tg_open["/data/static"][...]
     tg_dynamic = tg_open["/data/dynamic"]
-    if depermute:
-        inv_perm = tg_open["/data/permutation"][1]
-    else:
-        inv_perm = None
+    #if depermute:
+    #    inv_perm = tg_open["/data/permutation"][1]
+    #else:
+    #    inv_perm = None
     all_feats = [df[0] for df in derived_feats_serial] + list(tg_dlabels)
 
     ## determine sector slices along time axis
@@ -176,36 +187,46 @@ def _get_gs_mmsc(timegrid:Path, depermute=False, time_sector_size=None,
             derived_feats=dict(derived_feats_serial),
             )
 
+    ## go ahead and extract the full year for this spatial area and close file
+    #tg_dynamic = tg_dynamic[:,spatial_slice]
+    #tg_static = tg_static[spatial_slice]
+    #tg_open.close()
+
     ## construct a (M,T) counts array and (M,T,P,Fd,3) array for
     ## (min, max, sum) per combination of (month, ToD, pixel, feature)
-    mms = np.zeros((12,24,*tg_shape[1:3],3), dtype=np.float64)
+    if spatial_slice is None:
+        spatial_slice = slice(0,tg_shape[1])
+    mms_shape = (12,24,spatial_slice.stop-spatial_slice.start,tg_shape[2],3)
+    mms = np.full(mms_shape, np.nan, dtype=np.float64)
+    mms[-1] = 0 ## initialize sum to zero
     counts = np.zeros((12,24), dtype=int)
     for slc in tslices:
-        tmp_arrs = []
+        tinit = perf_counter()
         ## evaluate derived features and reorder stored feats as requested.
         tmpx = _calc_feat_array(
-                src_array=tg_dynamic[slc], ## (T, P, Fd)
-                static_array=tg_static, ## (P, Fs)
+                src_array=tg_dynamic[slc,spatial_slice], ## (T, P, Fd)
+                static_array=tg_static[spatial_slice], ## (P, Fs)
                 stored_feat_idxs=sfix,
                 derived_data=drv_info,
                 )
+        assert np.all(np.isfinite(tmpx)),(timegrid.name,spatial_slice,slc)
         midxs = tmp_months[slc]
         tidxs = tmp_tods[slc]
         for i in range(tmpx.shape[0]):
-            #mms[midxs[i],tidxs[i],:,:] =
             counts[midxs[i],tidxs[i]] += 1
             ## Determine min/max (P,F) for this timestep
-            mms[midxs[i],tidxs[i],:,:,0] = np.min([
+            mms[midxs[i],tidxs[i],:,:,0] = np.nanmin([
                 mms[midxs[i],tidxs[i],:,:,0], tmpx[i],
                 ], axis=0)
-            mms[midxs[i],tidxs[i],:,:,1] = np.max([
+            mms[midxs[i],tidxs[i],:,:,1] = np.nanmax([
                 mms[midxs[i],tidxs[i],:,:,1], tmpx[i],
                 ], axis=0)
             mms[midxs[i],tidxs[i],:,:,2] += tmpx[i]
-        print(counts)
         gc.collect()
-    if depermute:
-        mms = mms[:,:,inv_perm,:,:]
+        print(timegrid.name, spatial_slice, slc, f"{perf_counter()-tinit:.3f}")
+    #if depermute:
+    #    mms = mms[:,:,inv_perm,:,:]
+    tg_open.close()
     return (counts,mms)
 
 def par_var(n_a, mean_a, m2_a, n_b, mean_b, m2_b):
@@ -220,16 +241,34 @@ def par_var(n_a, mean_a, m2_a, n_b, mean_b, m2_b):
     return (n_ab, mean_ab, m2_ab)
 
 def make_gridstat_hdf5(timegrids:list, out_file:Path, depermute=True,
-        time_sector_size=None, derived_feats_serial:list=[], nworkers=1,
-        debug=False):
+        time_sector_size=None, space_sector_chunks=1,
+        derived_feats_serial:list=[], nworkers=1, debug=False):
     """
-    Calculate pixel-wise monthly min, max, mean, and standard deviation of each
-    stored dynamic and derived feature in the timegrids and store the
-    statistics alongside static data in a new hdf5 file.
+    Calculate pixel-wise monthly and time-wise min, max, value sum (for mean),
+    and variance sum of each stored dynamic and derived feature in the
+    timegrids and store the statistics alongside static data in a new hdf5.
+
+    The subsequent gridstats array has shape (12,24,P,F,4) corresponding to
+    axes for 12 months, 24 hours, P spatial pixels, F features (stored dynamic
+    or derived), and 4 metrics (min, max, meansum, varsum). A separate (12,24)
+    array of integer counts is stored in order to convert meansum and varsum
+    into true averages and variances per (month, ToD, pixel, feature) combo.
+
+    This is necessarily a 2-pass operation in order to get accurate variance
+    estimates; the first pass gets the min, max, and mean sum, then the second
+    pass uses the global means for each (month, ToD, pixel, feat) combo to
+    get the variance sum. It's important that a global mean across all
+    timegrids is utilized because the sample sizes are relatively small when
+    stratified across month and time of day.
 
     :@param timegrids: List of timegrids that cover the same spatial domain,
         which will all be incorporated into the pixelwise monthly calculations.
     :@param out_file: Path to a non-existing file to write gridstats to.
+    :@param depermute: If True, use the permutation stored in each timegrid
+        file to reorder the spatial indeces into their original positions.
+    :@param time_sector_size: Number of timesteps to extract per iteration
+    :@param space_sector_chunks: Number of spatial chunks (wrt the timegrid
+        chunk size) to pass to each worker for extraction.
     :@param derived_feats_serial: Provide a dict mapping NEW feature labels to
         a 3-tuple (dynamic_args, static_args, lambda_str) where the args are
         each tuples of existing dynamic/static labels, and lambda_str contains
@@ -247,6 +286,7 @@ def make_gridstat_hdf5(timegrids:list, out_file:Path, depermute=True,
         tg_open = h5py.File(tg, "r", rdcc_nbytes=128*1024**2, rdcc_nslots=256)
         if tg_shape is None:
             tg_shape = tg_open["/data/dynamic"].shape
+            tg_chunks = tg_open["/data/dynamic"].chunks
             ## collect dynamic and static feature labels
             tg_dlabels = json.loads(
                     tg_open["data"].attrs["dynamic"])["flabels"]
@@ -266,8 +306,7 @@ def make_gridstat_hdf5(timegrids:list, out_file:Path, depermute=True,
                     "Timegrid 2d valid mask must be uniform"
             assert tuple(tg_dlabels) == tuple(
                     json.loads(tg_open["data"].attrs["dynamic"])["flabels"])
-            if not depermute:
-                assert np.all(tg_perm == tg_open["/data/permutation"][...])
+            assert np.all(tg_perm == tg_open["/data/permutation"][...])
         tg_open.close()
 
     ## collect all feature labels, whether stored  or derived.
@@ -339,53 +378,108 @@ def make_gridstat_hdf5(timegrids:list, out_file:Path, depermute=True,
         "timegrids":json.dumps([p.name for p in timegrids]),
         })
 
+    ss_size = tg_chunks[1] * space_sector_chunks
+    ss_count = tg_shape[1] // ss_size + bool(tg_shape[1] % ss_size)
+    ss_0 = np.arange(ss_count) * ss_size
+    ss_f = np.r_[ss_0[1:], tg_shape[1]]
+    ss_slices = [slice(int(ix0),int(ixf)) for ix0,ixf in zip(ss_0, ss_f)]
+    print(f"{np.count_nonzero(tg_mask) = }")
+
     ## first pass: compute min, max, and sum values
     args = [{
-        "timegrid":tg, "depermute":depermute,
+        "timegrid":tg,
+        "spatial_slice":ss,
+        #"depermute":depermute,
         "time_sector_size":time_sector_size,
         "derived_feats_serial":derived_feats_serial,
-        } for tg in timegrids]
-
+        } for tg in timegrids for ss in ss_slices]
+    counts_per_tg = {}
     with Pool(nworkers) as pool:
         mms_total = np.full((*stats_shape[:-1], 3), np.nan)
-        counts_total = np.zeros((12,24))
-        for mms,counts in pool.imap_unordered(_mp_get_gs_mmsc, args):
-            if mms_total is None:
-                mms_total = mms
+        mms_total[...,2] = 0 ## initialize sum axis to zero
+        for a,(counts,mms) in pool.imap_unordered(_mp_get_gs_mmsc, args):
+            slc = a["spatial_slice"]
+            tgn = a["timegrid"].as_posix()
+            ## determine minimum and maximum
+            mms_total[:,:,slc,:,0] = np.nanmin([
+                mms_total[:,:,slc,:,0], mms[...,0]
+                ], axis=0)
+            mms_total[:,:,slc,:,1] = np.nanmax([
+                mms_total[:,:,slc,:,1], mms[...,1]
+                ], axis=0)
+            ## accumulate value sum
+            mms_total[:,:,slc,:,2] += mms[...,2]
+            ## counts should be redundant across spatial sectors per timegrid
+            if tgn in counts_per_tg.keys():
+                if not np.all(counts==counts_per_tg[tgn]):
+                    print(f"ERROR: {tgn} counts misalignment!")
+                    print(counts)
+                    print(counts_per_tg[tgn])
             else:
-                mms_total[...,0] = np.nanmin([
-                    mms_total[...,0], mms[...,0]
-                    ], axis=0)
-                mms_total[...,1] = np.nanmax([
-                    mms_total[...,1], mms[...,1]
-                    ], axis=0)
-                mms_total[...,2] += mms_total[...,2]
-                counts_total += counts
+                counts_per_tg[tgn] = counts
 
-    ## load the
-    G[:,:,:,:,:3] = mms_total
+    ## make sure the counts are also consistent wrt the mms array
+    counts_total = np.zeros((12,24))
+    for carr in counts_per_tg.values():
+        counts_total += carr
+
+    ## load the array into the new gridstats hdf5, depermuting if requested
+    if depermute:
+        G[:,:,:,:,:3] = mms_total[:,:,tg_perm[1]]
+    else:
+        G[:,:,:,:,:3] = mms_total
     C[...] = counts_total
     F.flush()
 
     ## second pass: compute standard deviation using mean values
     global_means = mms_total[...,2] / counts_total[:,:,np.newaxis,np.newaxis]
-    #global_means = np.zeros(stats_shape[:-1])
+
+    ## beg the garbage collector
+    del mms_total
+    gc.collect()
+
     args = [{
-        "timegrid":tg, "means":global_means, "depermute":depermute,
+        "timegrid":tg,
+        "means":global_means[:,:,ss],
+        "spatial_slice":ss,
+        #"depermute":depermute,
         "time_sector_size":time_sector_size,
         "derived_feats_serial":derived_feats_serial
-        } for tg in timegrids]
+        } for tg in timegrids for ss in ss_slices]
+    counts_per_tg_varsum = {}
     with Pool(nworkers) as pool:
         varsum_total = np.zeros(*stats_shape[:-1], 1)
-        counts_total_varsum = np.zeros((12,24))
-        for varsum,counts in pool.imap_unordered(_mp_get_gs_varsum, args):
-            varsum_total += varsum
-            counts_total_varsum += counts
+        for a,(counts,varsum) in pool.imap_unordered(_mp_get_gs_varsum, args):
+            slc = a["spatial_slice"]
+            tgn = a["timegrid"].as_posix()
+            varsum_total[:,:,slc,:,0] += varsum
+            #G[:,:,slc,:,3] += varsum
+            ## counts should be redundant across spatial sectors per timegrid
+            if tgn in counts_per_tg_varsum.keys():
+                if not np.all(counts==counts_per_tg_varsum[tgn]):
+                    print(f"ERROR: {tgn} counts misalignment!")
+                    print(counts)
+                    print(counts_per_tg_varsum[tgn])
+            else:
+                counts_per_tg_varsum[tgn] = counts
 
-    print(f"{np.all(counts_total == counts_total_varsum) = }")
-    G[:,:,:,:,3] = varsum
+    ## make sure the counts are also consistent wrt the mms array
+    counts_total_varsum = np.zeros((12,24))
+    for carr in counts_per_tg_varsum.values():
+        counts_total_varsum += carr
+    if not np.all(counts_total==counts_total_varsum):
+        print(f"ERROR: mms vs varsum counts misalignment!")
+        print(counts_total)
+        print(counts_total_varsum)
+
+    ## load the array into the new gridstats hdf5, depermuting if requested
+    if depermute:
+        G[:,:,:,:,3] = varsum[:,:,tg_perm[1]]
+    else:
+        G[:,:,:,:,3] = varsum
     F.flush()
     F.close()
+    return out_file
 
 if __name__=="__main__":
     data_dir = Path("/rstor/mdodson/era5")
@@ -402,10 +496,11 @@ if __name__=="__main__":
     make_gridstat_hdf5(
             timegrids=timegrids,
             out_file=gridstat_dir.joinpath(
-                f"gridstats_era5_2012-2023.h5"),
+                f"gridstats_era5_2012-2023_2.h5"),
             depermute=True,
             time_sector_size=24*14,
-            nworkers=6,
+            space_sector_chunks=16,
+            nworkers=12,
             debug=True,
             )
     #'''
